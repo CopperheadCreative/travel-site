6ff3f176aae26eeb1cacc5fc5f5e66259e915466
Bryan <bryan@copperheadcreative.ca>
2019-04-27 12:53:30 -0700

    automation of build task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
-gulp.task('watch', function(){
 
+gulp.task('watch', function(){
 	browserSync.init({
 		notify: false,
 		server: {
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
 require('./gulp/tasks/modernizer');
+require('./gulp/tasks/build');
 
 
 
